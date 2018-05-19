840947d90375972ce8bcf5898be548c9fed8d90b
Hayes Lee <hayeslee@Hayess-MacBook-Pro.local>
2018-05-18 22:23:49 -0400

    Updates
    Added context chats page, smooth scrolling, favicon on home page

--- script/main.js
+++ script/main.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
 
 	var colour;
-	$( "#researcher" ).hover(function() {
- 		$("#header-image").attr("src","assets/me-alternate.png");
-	});
 	$( "#avatar" ).click(function() {
 		var colours = ['#6ec6a5', '#8fc66e', '#bbc66e', '#796ec6'];
 		var randColour;
@@ -13,5 +10,26 @@ $(document).ready(function(){
 		colour = randColour;
 		$(".st9").attr("style", "fill:" + randColour);
 	});
+
+	$("a").on('click', function(event) {
+	    // Make sure this.hash has a value before overriding default behavior
+	    if (this.hash !== "") {
+	      // Prevent default anchor click behavior
+	      event.preventDefault();
+
+	      // Store hash
+	      var hash = this.hash;
+
+	      // Using jQuery's animate() method to add smooth page scroll
+	      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+	      $('html, body').animate({
+	        scrollTop: $(hash).offset().top
+	      }, 1000, function(){
+	   
+	        // Add hash (#) to URL when done scrolling (default click behavior)
+	        window.location.hash = hash;
+	      });
+	    } // End if
+  	});
 
 });
